220893b784b33810cb14af15a8c4d2661af0109b
hectorandac01 <hectorcuriso4@gmail.com>
2017-04-08 22:26:19 -0400

    Avanzado

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170408230624) do
 
-  create_table "authors", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "carrera"
-    t.string   "matricula"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "autors", force: :cascade do |t|
     t.string   "nombre"
     t.string   "apellido"
