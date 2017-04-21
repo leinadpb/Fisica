00a8e32ccf307fd73c47432fbec5b84002529313
leinadpb <daniel.pena@intec.edu.do>
2017-04-21 17:02:19 -0400

    Campo magnetico

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   get 'fisica/index'
   get 'fisica/campoelectrico'
   get 'fisica/authors'
-  get 'fisica/circuitos'
-
+  get 'fisica/magnetico'
   root 'fisica#authors'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
