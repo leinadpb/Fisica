54e58e430551534db8d92bb2214e6b00f3f26f58
hectorandac01 <hectorcuriso4@gmail.com>
2017-04-21 18:42:04 -0400

    Better performance

--- app/controllers/fisica_controller.rb
+++ app/controllers/fisica_controller.rb
@@ -4,4 +4,14 @@ class FisicaController < ApplicationController
     @daniel = Autor.find_by_nombre("Daniel")
     @users = Autor.all
   end
+
+  def index
+  end
+
+  def campoelectrico
+  end
+
+  def magnetico
+  end
+
 end
